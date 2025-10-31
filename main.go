6c74ba29657be440af3aa94640ae73a3f02f7643
Alexander <aalexanderbn@icloud.com>
2025-10-31 18:36:43 +0300

    fix

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 const fromUSDtoEUR float64 = 0.82
 const fromUSDtoRUB float64 = 82.0
+const fromEURtoRUB float64 = fromUSDtoRUB / fromUSDtoEUR
 
 func main() {
-	fmt.Println(fromUSDtoEUR, fromUSDtoRUB)
+	fmt.Println(fromEURtoRUB, fromUSDtoEUR, fromUSDtoRUB)
 }
