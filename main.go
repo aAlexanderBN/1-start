0c5ac1fb9a82f7941eda348f60aaa962a8acc605
Alexander <aalexanderbn@icloud.com>
2025-11-02 14:12:45 +0300

    домашка из 4ой части

--- main.go
+++ main.go
@@ -8,4 +8,24 @@ const fromEURtoRUB float64 = fromUSDtoRUB / fromUSDtoEUR
 
 func main() {
 	fmt.Println(fromEURtoRUB, fromUSDtoEUR, fromUSDtoRUB)
+	var sum float64
+	sum = readsum()
+
+	exchange(sum, "RUB", "EUR")
+	fmt.Println(sum)
+}
+
+func readsum() float64 {
+	var sum float64
+	fmt.Println("Введите сумму:")
+	_, er := fmt.Scan(&sum)
+	if er != nil {
+		fmt.Println("Ошибка ввода")
+	}
+	return sum
+}
+
+func exchange(sum float64, inputcurrency, outputcurrency string) float64 {
+
+	return sum
 }
